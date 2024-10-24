49533423a749b3324750c3602dddabf65b4f0215
Ervin_BH <ervin.academic@gmail.com>
2024-10-24 18:06:43 -0300

    Eliminar claves sensibles

--- config.py
+++ config.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 
 
-OPENAI_API_KEY = '------' # put your provided openai api key here
-ELEVENLABS_API_KEY = '' # https://www.elevenlabs.io/ - get free API key here
-VOICE_ID = '' # elevenlabs voice id
-DEEPGRAM_API_KEY = '' # https://www.deepgram.com/ - get free API key here
+# OPENAI_API_KEY # put your provided openai api key here
+# ELEVENLABS_API_KEY # https://www.elevenlabs.io/ - get free API key here
+# VOICE_ID # elevenlabs voice id
+# DEEPGRAM_API_KEY # https://www.deepgram.com/ - get free API key here
 
-SERPER_API_KEY = ''
+# SERPER_API_KEY
 
-# deepgram account: citehi9412@abaot.com, SAEDFakesdfnvfdev14143423
-# Elevenlabs account: citehi9412@abaot.com, M@ria123
+# deepgram account: 
+# Elevenlabs account: 
